2b74f328dc0a2a2e169834cba1706b636bc43274
Sid <shoutsid@gmail.com>
2023-10-16 22:25:29 +0100

    Move function registry into helpers folder

--- tests/agents/helpers/function_registry_test.py
+++ tests/agents/helpers/function_registry_test.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
-from agents.function_registry import FunctionRegistry
+from agents.helpers.function_registry import FunctionRegistry
 import pytest
 
 
