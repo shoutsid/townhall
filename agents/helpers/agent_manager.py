14f5701b07efce1568d5f7258f64d8620d85b28c
Sid <shoutsid@gmail.com>
2023-10-17 16:55:11 +0100

    Introduce AgentManage. A class to manage agents in a registry.

---
+++ agents/helpers/agent_manager.py
@@ -0,0 +1,68 @@
+class AgentManager:
+    """
+    A class to manage agents in a registry.
+
+    Attributes:
+      registry (dict): A dictionary to store agent details.
+
+    Methods:
+      register_agent: Registers an agent with the given ID and details.
+      unregister_agent: Unregisters an agent with the given ID.
+      query_agent: Returns the details of an agent with the given ID.
+      list_agents: Returns a dictionary of all registered agents and their details.
+    """
+
+    def __init__(self):
+        self.registry = {}
+
+    async def register_agent(self, agent_id, details):
+        """
+        Registers a new agent with the given ID and details.
+
+        Args:
+          agent_id (str): The ID of the agent to register.
+          details (dict): A dictionary containing details about the agent.
+
+        Returns:
+          str: A message indicating that the agent was registered successfully.
+        """
+        self.registry[agent_id] = {"details": details, "messages": []}
+        return f"Agent {agent_id} registered successfully."
+
+    async def unregister_agent(self, agent_id):
+        """
+        Unregister an agent from the agent manager.
+
+        Args:
+            agent_id (str): The ID of the agent to unregister.
+
+        Returns:
+            str: A message indicating whether the agent was successfully unregistered or not.
+        """
+        if agent_id in self.registry:
+            del self.registry[agent_id]
+            return f"Agent {agent_id} unregistered successfully."
+        else:
+            return f"Agent {agent_id} not found."
+
+    async def query_agent(self, agent_id):
+        """
+        Returns the details of the agent with the given ID.
+
+        Args:
+            agent_id (str): The ID of the agent to query.
+
+        Returns:
+            dict: A dictionary containing the details of the agent, or a string
+                  indicating that the agent was not found.
+        """
+        return self.registry.get(agent_id, {}).get(
+            "details", f"Agent {agent_id} not found."
+        )
+
+    async def list_agents(self):
+        """
+        Returns a dictionary containing the details of all registered agents.
+        The keys of the dictionary are the agent IDs, and the values are the agent details.
+        """
+        return {k: v["details"] for k, v in self.registry.items()}
---
+++ tests/agents/helpers/agent_manager_test.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import pytest
+
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+)
+
+from agents.helpers.agent_manager import AgentManager
+
+
+@pytest.mark.asyncio
+async def test_agent_management():
+    # Initialize AgentManager
+    agent_manager = AgentManager()
+
+    # Test agent registration
+    assert (
+        await agent_manager.register_agent(
+            "001", {"name": "Agent Smith", "status": "active"}
+        )
+        == "Agent 001 registered successfully."
+    )
+
+    # Test agent query
+    assert await agent_manager.query_agent("001") == {
+        "name": "Agent Smith",
+        "status": "active",
+    }
+
+    # Test agent unregistration
+    assert (
+        await agent_manager.unregister_agent("001")
+        == "Agent 001 unregistered successfully."
+    )
+
+    # Test agent query after unregistration
+    assert await agent_manager.query_agent("001") == "Agent 001 not found."
