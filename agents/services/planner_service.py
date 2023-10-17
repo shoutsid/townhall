40a46f059e3ce19986d8d6272c80b644abc93f9a
Sid <shoutsid@gmail.com>
2023-10-17 17:44:48 +0100

    docs: Document planner service

--- agents/services/planner_service.py
+++ agents/services/planner_service.py
@@ -10,6 +10,14 @@ from autogen import AssistantAgent, UserProxyAgent
 
 
 class PlannerService:
+    """
+    A service class that provides an interface for interacting with the planner agent.
+
+    Attributes:
+      planner (AssistantAgent): The planner agent used for generating plans.
+      planner_user (UserProxyAgent): The user proxy agent used for communicating with the planner agent.
+    """
+
     def __init__(self, config_list):
         self.planner = AssistantAgent(
             name="planner", llm_config={"config_list": config_list}
@@ -19,5 +27,14 @@ class PlannerService:
         )
 
     def ask_planner(self, msg):
+        """
+        Sends a message to the planner and returns the response.
+
+        Args:
+          msg (str): The message to send to the planner.
+
+        Returns:
+          str: The response from the planner.
+        """
         self.planner_user.initiate_chat(self.planner, message=msg)
         return self.planner_user.last_message()["content"]
