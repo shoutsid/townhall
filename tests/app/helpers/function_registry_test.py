fae2422954092c737b17b9ae1b821cb92c624c25
Sid <shoutsid@gmail.com>
2023-10-18 19:22:58 +0100

    (issue14) Move function_registry to respective app/helpers folder

--- tests/app/helpers/function_registry_test.py
+++ tests/app/helpers/function_registry_test.py
@@ -1,13 +1,13 @@
+import pytest
+from app.helpers.function_registry import FunctionRegistry
 import sys
 import os
 
 
 sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+    0, os.path.abspath(os.path.join(
+        os.path.dirname(__file__), "..", "..", ".."))
 )
-
-from agents.helpers.function_registry import FunctionRegistry
-import pytest
 
 
 class TestFunctionRegistry:
