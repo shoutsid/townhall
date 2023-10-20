4a716978948dc9d603c1df3545c896a3ffbfb96d
Sid <shoutsid@gmail.com>
2023-10-20 02:41:47 +0100

    Update Python version in README and refactor DQN model to use tinygrad
    - Update required Python version to 3.10 in README.md
    - Refactor DQN model in app/models/dqn.py to use tinygrad instead of PyTorch
    - Update corresponding tests in tests/app/models/dqn_test.py to align with tinygrad usage

--- app/models/dqn.py
+++ app/models/dqn.py
@@ -1,12 +1,11 @@
 """
 DQN model class, based on the DQN model from the PyTorch tutorial:
 """
-import torch
-import torch.nn as nn
 from typing import List
+from tinygrad.tensor import Tensor
 
 
-class DQN(nn.Module):
+class DQN:
     """
     Deep Q-Network (DQN) class.
 
@@ -16,8 +15,8 @@ class DQN(nn.Module):
         output_size (int): The number of output units.
 
     Attributes:
-        layers (nn.ModuleList): The list of hidden layers.
-        output_layer (nn.Linear): The output layer.
+        layers (List[Tensor]): The list of hidden layers.
+        output_layer (Tensor): The output layer.
 
     Methods:
         forward(x): Forward pass through the network.
@@ -25,30 +24,29 @@ class DQN(nn.Module):
     """
 
     def __init__(self, input_size: int, hidden_sizes: List[int] = None, output_size: int = 1):
-        super().__init__()
         if hidden_sizes is None:
             hidden_sizes = [32, 16]
-        self.layers = nn.ModuleList()
+        self.layers = []
         in_size = input_size
 
         # Create hidden layers
         for size in hidden_sizes:
-            self.layers.append(nn.Linear(in_size, size))
+            self.layers.append(Tensor.uniform(in_size, size))
             in_size = size
 
         # Create output layer
-        self.output_layer = nn.Linear(in_size, output_size)
+        self.output_layer = Tensor.uniform(in_size, output_size)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         """
         Forward pass of the DQN network.
 
         Args:
-            x (torch.Tensor): Input tensor of shape (batch_size, input_size).
+            x (Tensor): Input tensor of shape (batch_size, input_size).
 
         Returns:
-            torch.Tensor: Output tensor of shape (batch_size, num_actions).
+            Tensor: Output tensor of shape (batch_size, num_actions).
         """
         for layer in self.layers:
-            x = torch.relu(layer(x))
-        return self.output_layer(x)
+            x = x.dot(layer).relu()
+        return x.dot(self.output_layer)
--- tests/app/models/dqn_test.py
+++ tests/app/models/dqn_test.py
@@ -1,30 +1,37 @@
-import torch
+from tinygrad.tensor import Tensor
 from app.models.dqn import DQN
 
 
-def test_dqn_forward():
-    """
-    Test the forward method of the DQN model.
+def test_dqn_forward_pass():
+    # Create a DQN with 2 input features, 2 hidden layers with 3 and 4 units, and 2 output units
+    dqn = DQN(input_size=2, hidden_sizes=[3, 4], output_size=2)
 
-    Creates a DQN model with 2 input features, 2 hidden layers with 3 and 4 units, and 1 output unit.
-    Creates a batch of 2 input tensors of shape (2,).
-    Passes the input through the model.
-    Checks that the output has shape (2, 1).
-    """
-    model = DQN(input_size=2, hidden_sizes=[3, 4], output_size=1)
-    x = torch.tensor([[1.0, 2.0], [3.0, 4.0]])
-    output = model.forward(x)
-    assert output.shape == (2, 1)
+    # Create an input tensor of shape (1, 2)
+    x = Tensor([[1.0, 2.0]])
+
+    # Compute the output of the DQN for the input tensor
+    y = dqn.forward(x)
+
+    # Check that the output tensor has shape (1, 2)
+    assert y.shape == (1, 2)
 
 
-def test_dqn_output():
-    """
-    Test the output of a DQN model with 2 input features, 2 hidden layers with 3 and 4 units, and 1 output unit.
+def test_dqn_default_hidden_sizes():
+    # Create a DQN with 2 input features and default hidden layer sizes
+    dqn = DQN(input_size=2)
 
-    Creates a DQN model with the specified architecture, generates a batch of 2 input tensors of shape (2,),
-    passes the input through the model, and checks that the output values are between -1 and 1.
-    """
-    model = DQN(input_size=2, hidden_sizes=[3, 4], output_size=1)
-    x = torch.tensor([[1.0, 2.0], [3.0, 4.0]])
-    output = model.forward(x)
-    assert torch.all(output >= -1.0) and torch.all(output <= 1.0)
+    # Check that the DQN has 2 hidden layers with sizes 32 and 16
+    assert len(dqn.layers) == 2
+    assert dqn.layers[0].shape == (2, 32)
+    assert dqn.layers[1].shape == (32, 16)
+
+
+def test_dqn_custom_hidden_sizes():
+    # Create a DQN with 2 input features and custom hidden layer sizes
+    dqn = DQN(input_size=2, hidden_sizes=[4, 5, 6])
+
+    # Check that the DQN has 3 hidden layers with sizes 4, 5, and 6
+    assert len(dqn.layers) == 3
+    assert dqn.layers[0].shape == (2, 4)
+    assert dqn.layers[1].shape == (4, 5)
+    assert dqn.layers[2].shape == (5, 6)
