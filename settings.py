9c54531ec9fc9c52bcbd37ae4b512f3df7d58bb2
Sid <shoutsid@gmail.com>
2023-10-22 18:17:41 +0100

    bug: correct GPT4 configurable.

--- settings.py
+++ settings.py
@@ -12,26 +12,26 @@ GPT3_5_TURBO_0613 = "gpt-3.5-turbo-0613"
 GPT4_0613 = "gpt-4-0613"
 VICUNA_7B_V1_5 = "vicuna-7b-v1.5"
 
-if "gpt4" in os.environ:
-    CONFIG_LIST = [
-        {
-            "model": GPT3_5_TURBO_0613,
-            "api_key": OPENAI_API_KEY,
-        }
-    ]
-else:
+if "GPT4" in os.environ:
     CONFIG_LIST = [
         {
             "model": GPT4_0613,
             "api_key": OPENAI_API_KEY,
         }
     ]
-
-LOCAL_CONFIG_LIST = [
-    {
-        "model": GPT3_5_TURBO_0613,
-        "api_key": "None",
-        "api_base": "http://localhost:8000/v1",
-        "api_type": "open_ai",
-    }
-]
+elif "LOCAL" in os.environ:
+    CONFIG_LIST = [
+        {
+            "model": GPT3_5_TURBO_0613,
+            "api_key": "None",
+            "api_base": "http://localhost:8000/v1",
+            "api_type": "open_ai",
+        }
+    ]
+else:
+    CONFIG_LIST = [
+        {
+            "model": GPT3_5_TURBO_0613,
+            "api_key": OPENAI_API_KEY,
+        }
+    ]
